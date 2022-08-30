2d6b4ca40e23f8b4106df4679550c444a0add844
root <root@kali.azatezalo>
2022-08-29 22:47:46 -0400

    Backend functionality 75% Done. Formula for BF% needs finishing, and neck field needs adding. Also we need Icon.

--- app/src/main/java/com/example/bodyfatpercentagecalculator/MainActivity.java
+++ app/src/main/java/com/example/bodyfatpercentagecalculator/MainActivity.java
@@ -3,12 +3,95 @@ package com.example.bodyfatpercentagecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText waist;
+    EditText height;
+    EditText hip;
+    Switch isMale;
+    Switch isFemale;
+    Button calculate;
+    EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        // getting all items
+        waist = findViewById(R.id.waistWidth);
+        hip = findViewById(R.id.hipWidth);
+        height = findViewById(R.id.height);
+        isMale  = (Switch) findViewById(R.id.isMale);
+        isFemale =  (Switch) findViewById(R.id.isFemale);
+        calculate = findViewById(R.id.calculate);
+        result = findViewById(R.id.result);
+
+        //Setting Switches and Toggoling
+        isMale.setChecked(false);
+        isFemale.setChecked(true);
+
+        isMale.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                if (isMale.isChecked()){
+                   isFemale.setChecked(false);
+                } else {
+                    isFemale.setChecked(true);
+                }//checking if  switch is checked
+            }
+        });
+
+        isFemale.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                if (isFemale.isChecked()){
+                    isMale.setChecked(false);
+                } else {
+                    isMale.setChecked(true);
+
+                } //checking if  switch is checked
+            }
+        });
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sWaist = waist.getText().toString();
+                String sHip = hip.getText().toString();
+                String sHeight = height.getText().toString();
+
+                if (isFemale.isChecked()){
+                    if (sWaist.matches("") | sHip.matches("") | sHeight.matches("")) {
+                        Toast.makeText(getApplicationContext(), "Fill Out Waist, Hip, and Height.",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        result.setText("11");
+                    }
+                }
+
+                if (isMale.isChecked()){
+                    if (sHip.matches("") | sHeight.matches("")) {
+                        Toast.makeText(getApplicationContext(), "Fill Out Hip, and Height.",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        result.setText("12");
+                    }
+                }
+
+            }
+        });
+
     }
+
+
+
 }
